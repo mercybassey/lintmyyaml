2774c4ed3b18811fd3cbf0fc619359043e3ca5d4
mercybassey <udohmercy911@gmail.com>
2024-01-05 13:58:13 +0100

    Configured the tool to take more tha one arguments

--- cli.py
+++ cli.py
@@ -3,10 +3,11 @@ from file.reading_and_writing import read_and_write_file
 
 def main():
     parser = argparse.ArgumentParser(description='YAML Correction CLI Tool')
-    parser.add_argument('filename', help='Input YAML file name without extension(both ."yml" and ".yaml" extensions are supported)')
+    parser.add_argument('filenames', nargs='+', help='Input YAML file name without extension(both ."yml" and ".yaml" extensions are supported)')
     args = parser.parse_args()
 
-    read_and_write_file(args.filename)
+    for filename in args.filenames:
+        read_and_write_file(filename)
 
 if __name__ == '__main__':
     main()
